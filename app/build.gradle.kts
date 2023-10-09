63ce01c31695cb6cd5f63cf4fa2cc816fe21842f
FakeException <francone917@gmail.com>
2023-10-09 19:51:11 +0200

    Proguard fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.robuxearny.official"
         minSdk = 24
         targetSdk = 33
-        versionCode = 24
+        versionCode = 25
         versionName = "2.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
